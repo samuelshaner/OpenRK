dff5cb02a8a73e2fc3f516fb320dd42a42acf081
Will Boyd <wbinventor@gmail.com>
2014-03-22 19:33:41 -0400

    Initialized .gitignore and added simple pin cell OpenMOC/OpenMC compatibility test file

---
+++ openmc-compat/pin-cell/pin-cell.py
@@ -0,0 +1,80 @@
+from openmoc import *
+import openmoc.log as log
+import openmoc.materialize as materialize
+import openmoc.compatible.openmc as openmc
+
+###############################################################################
+###########################   Creating Materials   ############################
+###############################################################################
+
+log.py_printf('NORMAL', 'Importing materials data from HDF5...')
+
+materials = materialize.materialize('../c5g7-materials.h5')
+
+uo2_id = materials['UO2'].getId()
+water_id = materials['Water'].getId()
+
+
+###############################################################################
+###########################   Creating Surfaces   #############################
+###############################################################################
+
+log.py_printf('NORMAL', 'Creating surfaces...')
+
+circle = Circle(x=0.0, y=0.0, radius=1.0)
+left = XPlane(x=-2.0)
+right = XPlane(x=2.0)
+top = YPlane(y=2.0)
+bottom = YPlane(y=-2.0)
+
+left.setBoundaryType(REFLECTIVE)
+right.setBoundaryType(REFLECTIVE)
+top.setBoundaryType(REFLECTIVE)
+bottom.setBoundaryType(REFLECTIVE)
+
+
+###############################################################################
+#############################   Creating Cells   ##############################
+###############################################################################
+
+log.py_printf('NORMAL', 'Creating cells...')
+
+cells = []
+cells.append(CellBasic(universe=1, material=uo2_id))
+cells.append(CellBasic(universe=1, material=water_id))
+cells.append(CellFill(universe=0, universe_fill=2))
+
+cells[0].addSurface(halfspace=-1, surface=circle)
+cells[1].addSurface(halfspace=+1, surface=circle)
+cells[2].addSurface(halfspace=+1, surface=left)
+cells[2].addSurface(halfspace=-1, surface=right)
+cells[2].addSurface(halfspace=+1, surface=bottom)
+cells[2].addSurface(halfspace=-1, surface=top)
+
+
+###############################################################################
+###########################   Creating Lattices   #############################
+###############################################################################
+
+log.py_printf('NORMAL', 'Creating simple pin cell lattice...')
+
+lattice = Lattice(id=2, width_x=16.0, width_y=16.0)
+lattice.setLatticeCells([[1,1],
+                         [1,1]])
+
+
+###############################################################################
+##########################   Creating the Geometry   ##########################
+###############################################################################
+
+log.py_printf('NORMAL', 'Creating geometry...')
+
+geometry = Geometry()
+for material in materials.values(): geometry.addMaterial(material)
+for cell in cells: geometry.addCell(cell)
+geometry.addLattice(lattice)
+
+
+
+
+openmc.create_geometry_file(geometry)
