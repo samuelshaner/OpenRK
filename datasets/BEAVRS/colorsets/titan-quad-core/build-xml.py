856077b230d8975cb38fa0140307142e96968d99
Will Boyd <wbinventor@gmail.com>
2014-09-04 16:11:19 -0400

    Added titan quad core dataset

---
+++ datasets/BEAVRS/colorsets/titan-quad-core/build-xml.py
@@ -0,0 +1,86 @@
+from datasets.energy_groups import group_structures
+from openmc import *
+from datasets.BEAVRS.materials import openmc_materials, nuclides
+from geometry import geometry
+from infermc.build import *
+
+
+###############################################################################
+###################   Simulation Input File Parameters   ######################
+###############################################################################
+
+# OpenMC simulation parameters
+batches = 250
+inactive = 50
+particles = 100000
+
+
+###############################################################################
+##################   Exporting to OpenMC geometry.xml File  ###################
+###############################################################################
+
+# Create geometry.xml
+openmc_geometry = get_openmc_geometry(geometry)
+geometry_file = openmc.GeometryFile()
+geometry_file.setGeometry(openmc_geometry)
+geometry_file.exportToXML()
+
+
+###############################################################################
+##################   Exporting to OpenMC materials.xml File  ##################
+###############################################################################
+
+materials_file = MaterialsFile()
+materials_file.addMaterials(openmc_materials.values())
+materials_file.exportToXML()
+
+
+###############################################################################
+##################   Exporting to OpenMC settings.xml File  ###################
+###############################################################################
+
+settings_file = SettingsFile()
+settings_file.setBatches(batches)
+settings_file.setInactive(inactive)
+settings_file.setParticles(particles)
+settings_file.setStatepointInterval(5)
+settings_file.setOutput({'tallies': False})
+source_bounds = [geometry.getMinX(), geometry.getMinY(), geometry.getMinZ(), \
+                 geometry.getMaxX(), geometry.getMaxY(), geometry.getMaxZ()]
+settings_file.setSourceSpace('box', source_bounds)
+settings_file.setThreads(8)
+settings_file.exportToXML()
+
+
+###############################################################################
+##################   Exporting to OpenMC plots.xml File  ######################
+###############################################################################
+
+plot = Plot(plot_id=1)
+plot.setWidth(width=[geometry.getMaxX()-geometry.getMinX(),
+                     geometry.getMaxY()-geometry.getMinY()])
+plot_file = PlotsFile()
+plot_file.addPlot(plot)
+plot_file.exportToXML()
+
+
+###############################################################################
+##################   Exporting to OpenMC tallies.xml File  ####################
+###############################################################################
+
+tally_factory = XSTallyFactory(openmc_geometry)
+micro_tally_factory = MicroXSTallyFactory(openmc_geometry)
+
+groups = group_structures['CASMO']['2-group']
+
+nuclides = openmc_geometry.getAllNuclides()
+tally_nuclides = [nuclides['H-1'], nuclides['B-10'], nuclides['O-16'],
+                  nuclides['U-235'], nuclides['U-238'], nuclides['Zr-90'],
+                  nuclides['Fe-56']]
+
+micro_tally_factory.createAllMultiGroupXS(groups, domain_type='distribcell',
+                                          nuclides=tally_nuclides)
+micro_tally_factory.createAllMultiGroupXS(groups, domain_type='cell',
+                                          nuclides=tally_nuclides)
+
+micro_tally_factory.createTalliesFile()
