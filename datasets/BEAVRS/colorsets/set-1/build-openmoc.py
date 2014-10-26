cbf9c49e40f4518c9b101d0af71d7c7968890c96
Will Boyd <wbinventor@gmail.com>
2014-10-26 12:51:35 -0400

    Added initial OpenCSG/OpenMOC compatibility module files

---
+++ datasets/BEAVRS/colorsets/set-1/build-openmoc.py
@@ -0,0 +1,13 @@
+from openmoc.compatible.opencsg_compatible import *
+from geometry import geometry
+
+
+###############################################################################
+##################   Exporting to OpenMC geometry.xml File  ###################
+###############################################################################
+
+openmoc_geometry = get_openmoc_geometry(geometry)
+openmoc_geometry.initializeFlatSourceRegions()
+
+import openmoc.plotter as plotter
+plotter.plot_cells(openmoc_geometry)
