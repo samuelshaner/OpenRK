f593c4da88228f4e2b3dd3f9d488881c5509396f
Will Boyd <wbinventor@gmail.com>
2014-11-21 11:55:18 -0500

    Fixed bug in pin cells

--- datasets/BEAVRS/pin_cells.py
+++ datasets/BEAVRS/pin_cells.py
@@ -1,6 +1,6 @@
 from opencg import Universe, Cell
 from datasets.BEAVRS.surfaces import *
-from datasets.BEAVRS.materials import opencsg_materials
+from datasets.BEAVRS.materials import opencg_materials
 
 # NOTE - These are all taken at axial locations above the dashpot.
 # NOTE - The guide tubes are assumed to be empty.
@@ -19,21 +19,21 @@ pin_cells = dict()
 universe = Universe(name='1.6% Fuel Pin')
 pin_cells[universe._name] = universe
 
-cell = Cell(name='Fuel', fill=opencsg_materials['1.6% Fuel'])
+cell = Cell(name='Fuel', fill=opencg_materials['1.6% Fuel'])
 cell.addSurface(surface=surfaces['Fuel Radius-1'], halfspace=-1)
 universe.addCell(cell)
 
-cell = Cell(name='Gap', fill=opencsg_materials['Helium'])
+cell = Cell(name='Gap', fill=opencg_materials['Helium'])
 cell.addSurface(surface=surfaces['Fuel Radius-1'], halfspace=+1)
 cell.addSurface(surface=surfaces['Fuel Radius-2'], halfspace=-1)
 universe.addCell(cell)
 
-cell = Cell(name='Clad', fill=opencsg_materials['Zircaloy'])
+cell = Cell(name='Clad', fill=opencg_materials['Zircaloy'])
 cell.addSurface(surface=surfaces['Fuel Radius-2'], halfspace=+1)
 cell.addSurface(surface=surfaces['Fuel Radius-3'], halfspace=-1)
 universe.addCell(cell)
 
-cell = Cell(name='Moderator', fill=opencsg_materials['Borated Water'])
+cell = Cell(name='Moderator', fill=opencg_materials['Borated Water'])
 cell.addSurface(surface=surfaces['Fuel Radius-3'], halfspace=+1)
 universe.addCell(cell)
 
@@ -45,21 +45,21 @@ universe.addCell(cell)
 universe = Universe(name='2.4% Fuel Pin')
 pin_cells[universe._name] = universe
 
-cell = Cell(name='Fuel', fill=opencsg_materials['2.4% Fuel'])
+cell = Cell(name='Fuel', fill=opencg_materials['2.4% Fuel'])
 cell.addSurface(surface=surfaces['Fuel Radius-1'], halfspace=-1)
 universe.addCell(cell)
 
-cell = Cell(name='Gap', fill=opencsg_materials['Helium'])
+cell = Cell(name='Gap', fill=opencg_materials['Helium'])
 cell.addSurface(surface=surfaces['Fuel Radius-1'], halfspace=+1)
 cell.addSurface(surface=surfaces['Fuel Radius-2'], halfspace=-1)
 universe.addCell(cell)
 
-cell = Cell(name='Clad', fill=opencsg_materials['Zircaloy'])
+cell = Cell(name='Clad', fill=opencg_materials['Zircaloy'])
 cell.addSurface(surface=surfaces['Fuel Radius-2'], halfspace=+1)
 cell.addSurface(surface=surfaces['Fuel Radius-3'], halfspace=-1)
 universe.addCell(cell)
 
-cell = Cell(name='Moderator', fill=opencsg_materials['Borated Water'])
+cell = Cell(name='Moderator', fill=opencg_materials['Borated Water'])
 cell.addSurface(surface=surfaces['Fuel Radius-3'], halfspace=+1)
 universe.addCell(cell)
 
@@ -71,21 +71,21 @@ universe.addCell(cell)
 universe = Universe(name='3.1% Fuel Pin')
 pin_cells[universe._name] = universe
 
-cell = Cell(name='Fuel', fill=opencsg_materials['3.1% Fuel'])
+cell = Cell(name='Fuel', fill=opencg_materials['3.1% Fuel'])
 cell.addSurface(surface=surfaces['Fuel Radius-1'], halfspace=-1)
 universe.addCell(cell)
 
-cell = Cell(name='Gap', fill=opencsg_materials['Helium'])
+cell = Cell(name='Gap', fill=opencg_materials['Helium'])
 cell.addSurface(surface=surfaces['Fuel Radius-1'], halfspace=+1)
 cell.addSurface(surface=surfaces['Fuel Radius-2'], halfspace=-1)
 universe.addCell(cell)
 
-cell = Cell(name='Clad', fill=opencsg_materials['Zircaloy'])
+cell = Cell(name='Clad', fill=opencg_materials['Zircaloy'])
 cell.addSurface(surface=surfaces['Fuel Radius-2'], halfspace=+1)
 cell.addSurface(surface=surfaces['Fuel Radius-3'], halfspace=-1)
 universe.addCell(cell)
 
-cell = Cell(name='Moderator', fill=opencsg_materials['Borated Water'])
+cell = Cell(name='Moderator', fill=opencg_materials['Borated Water'])
 cell.addSurface(surface=surfaces['Fuel Radius-3'], halfspace=+1)
 universe.addCell(cell)
 
@@ -97,16 +97,16 @@ universe.addCell(cell)
 universe = Universe(name='Guide Tube')
 pin_cells[universe._name] = universe
 
-cell = Cell(name='Inner Water', fill=opencsg_materials['Borated Water'])
+cell = Cell(name='Inner Water', fill=opencg_materials['Borated Water'])
 cell.addSurface(surface=surfaces['GT Radius-1'], halfspace=-1)
 universe.addCell(cell)
 
-cell = Cell(name='Clad', fill=opencsg_materials['Zircaloy'])
+cell = Cell(name='Clad', fill=opencg_materials['Zircaloy'])
 cell.addSurface(surface=surfaces['GT Radius-1'], halfspace=+1)
 cell.addSurface(surface=surfaces['GT Radius-2'], halfspace=-1)
 universe.addCell(cell)
 
-cell = Cell(name='Outer Water', fill=opencsg_materials['Borated Water'])
+cell = Cell(name='Outer Water', fill=opencg_materials['Borated Water'])
 cell.addSurface(surface=surfaces['GT Radius-2'], halfspace=+1)
 universe.addCell(cell)
 
@@ -118,16 +118,16 @@ universe.addCell(cell)
 universe = Universe(name='Instrument Tube')
 pin_cells[universe._name] = universe
 
-cell = Cell(name='Air', fill=opencsg_materials['Air'])
+cell = Cell(name='Air', fill=opencg_materials['Air'])
 cell.addSurface(surface=surfaces['IT Radius-1'], halfspace=-1)
 universe.addCell(cell)
 
-cell = Cell(name='Clad', fill=opencsg_materials['Zircaloy'])
+cell = Cell(name='Clad', fill=opencg_materials['Zircaloy'])
 cell.addSurface(surface=surfaces['IT Radius-1'], halfspace=+1)
 cell.addSurface(surface=surfaces['IT Radius-2'], halfspace=-1)
 universe.addCell(cell)
 
-cell = Cell(name='Moderator', fill=opencsg_materials['Borated Water'])
+cell = Cell(name='Moderator', fill=opencg_materials['Borated Water'])
 cell.addSurface(surface=surfaces['IT Radius-2'], halfspace=+1)
 universe.addCell(cell)
 
@@ -139,45 +139,45 @@ universe.addCell(cell)
 universe = Universe(name='Burnable Absorber')
 pin_cells[universe._name] = universe
 
-cell = Cell(name='Region-1', fill=opencsg_materials['Air'])
+cell = Cell(name='Region-1', fill=opencg_materials['Air'])
 cell.addSurface(surface=surfaces['BA Radius-1'], halfspace=-1)
 universe.addCell(cell)
 
-cell = Cell(name='Region-2', fill=opencsg_materials['Steel'])
+cell = Cell(name='Region-2', fill=opencg_materials['Steel'])
 cell.addSurface(surface=surfaces['BA Radius-1'], halfspace=+1)
 cell.addSurface(surface=surfaces['BA Radius-2'], halfspace=-1)
 universe.addCell(cell)
 
-cell = Cell(name='Region-3', fill=opencsg_materials['Air'])
+cell = Cell(name='Region-3', fill=opencg_materials['Air'])
 cell.addSurface(surface=surfaces['BA Radius-2'], halfspace=+1)
 cell.addSurface(surface=surfaces['BA Radius-3'], halfspace=-1)
 universe.addCell(cell)
 
-cell = Cell(name='Region-4', fill=opencsg_materials['Boro. Glass'])
+cell = Cell(name='Region-4', fill=opencg_materials['Boro. Glass'])
 cell.addSurface(surface=surfaces['BA Radius-3'], halfspace=+1)
 cell.addSurface(surface=surfaces['BA Radius-4'], halfspace=-1)
 universe.addCell(cell)
 
-cell = Cell(name='Region-5', fill=opencsg_materials['Air'])
+cell = Cell(name='Region-5', fill=opencg_materials['Air'])
 cell.addSurface(surface=surfaces['BA Radius-4'], halfspace=+1)
 cell.addSurface(surface=surfaces['BA Radius-5'], halfspace=-1)
 universe.addCell(cell)
 
-cell = Cell(name='Region-6', fill=opencsg_materials['Steel'])
+cell = Cell(name='Region-6', fill=opencg_materials['Steel'])
 cell.addSurface(surface=surfaces['BA Radius-5'], halfspace=+1)
 cell.addSurface(surface=surfaces['BA Radius-6'], halfspace=-1)
 universe.addCell(cell)
 
-cell = Cell(name='Region-7', fill=opencsg_materials['Borated Water'])
+cell = Cell(name='Region-7', fill=opencg_materials['Borated Water'])
 cell.addSurface(surface=surfaces['BA Radius-6'], halfspace=+1)
 cell.addSurface(surface=surfaces['BA Radius-7'], halfspace=-1)
 universe.addCell(cell)
 
-cell = Cell(name='Region-8', fill=opencsg_materials['Zircaloy'])
+cell = Cell(name='Region-8', fill=opencg_materials['Zircaloy'])
 cell.addSurface(surface=surfaces['BA Radius-7'], halfspace=+1)
 cell.addSurface(surface=surfaces['BA Radius-8'], halfspace=-1)
 universe.addCell(cell)
 
-cell = Cell(name='Region-9', fill=opencsg_materials['Borated Water'])
+cell = Cell(name='Region-9', fill=opencg_materials['Borated Water'])
 cell.addSurface(surface=surfaces['BA Radius-8'], halfspace=+1)
 universe.addCell(cell)
