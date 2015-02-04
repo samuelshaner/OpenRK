6633ef7b65796db94daa5dcd8692a822b7bef6c6
Will Boyd <wbinventor@gmail.com>
2015-02-04 14:55:01 -0500

    Fixed bug in inf medium group sweep

--- datasets/BEAVRS/inf-medium/group-sweep-openmoc.py
+++ datasets/BEAVRS/inf-medium/group-sweep-openmoc.py
@@ -18,7 +18,7 @@ import numpy, h5py
 # OpenMC simulation parameters
 batches = 100
 inactive = 5
-particles = 10000
+particles = 100
 structures = [2,4,8,12,16,25] #,40,70]
 
 # Initialize array to contain all data
@@ -129,6 +129,8 @@ for i, num_groups in enumerate(structures):
 
   print('testing {0} groups'.format(num_groups))
 
+  groups = group_structures['CASMO']['{0}-group'.format(num_groups)]
+
   ########################   Extracting Cross-Sections  ########################
 
   # Initialize handle on the OpenMC summary file
@@ -152,7 +154,7 @@ for i, num_groups in enumerate(structures):
 
     # DUMP-TO-FILE and PRINT XS
     filename = 'mgxs-batch-{0}-groups-{1}'.format(batch, num_groups)
-
+    
     for material in materials:
       for xs_type in xs_types:
         xs = micro_extractor._multigroup_xs['material'][material._id][xs_type]
@@ -204,6 +206,8 @@ for i, num_groups in enumerate(structures):
         for rxn_type in nuclide_group.keys():
           if rxn_type in macro_xs.keys():
             micro_xs = nuclide_group[rxn_type]['average'][...]
+            print type(micro_xs), type(density)
+            print len(micro_xs), len(micro_xs)
             macro_xs[rxn_type] += micro_xs * density
 
       #########################  Create OpenMOC Materials  #######################
